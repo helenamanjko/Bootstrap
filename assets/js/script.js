20bb9959636f47d0b1c622e232cef308c4a7c52d
helenamanjko <helenamanjko@gmail.com>
2021-06-22 16:03:22 -0300

    subindo alteracao

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,20 @@
 let nome = document.querySelector("#nome");
 let email = document.querySelector("#email");
 let assunto = document.querySelector("#assunto");
+let senha = document.querySelector("#senha");
+let address = document.querySelector("#address");
+let cidade = document.querySelector("#cidade");
+let CEP = document.querySelector("#CEP");
+let estado = document.querySelector("#estado");
 
 let nomeOk = false;
 let emailOk = false;
 let assuntoOk = false;
+let senhaOk = false;
+let enderecolOk = false;
+let cidadeOk = false;
+let cepOk = false;
+let estadoOk = false;
 
 function validaNome() {
   let txtNome = document.querySelector("#txtNome");
@@ -42,10 +52,101 @@ function validaAssunto() {
     assuntoOk = true;
   }
 }
+function validaSenha() {
+  let txtSenha = document.querySelector("#txtSenha");
+
+  if (senha.value.length < 6) {
+    txtSenha.innerHTML = "Mínimo de 6 digitos";
+    txtSenha.style.color = "red";
+  } else {
+    txtSenha.innerHTML = "Senha válida";
+    txtSenha.style.color = "green";
+    senhaOk = true;
+  }
+}
+function validaEndereco() {
+  let txtEndereco = document.querySelector("#txtEndereco");
+
+  if (address.value == "") {
+    txtEndereco.innerHTML = "Endereço inválido";
+    txtEndereco.style.color = "red";
+  } else {
+    txtEndereco.innerHTML = "Endereço válido";
+    txtEndereco.style.color = "green";
+    enderecolOk = true;
+  }
+}
+function validaCidade() {
+  let txtCidade = document.querySelector("#txtCidade");
+
+  if (cidade.value == "") {
+    txtCidade.innerHTML = "Cidade inválida";
+    txtCidade.style.color = "red";
+  } else {
+    txtCidade.innerHTML = "Cidade válida";
+    txtCidade.style.color = "green";
+    cidadeOk = true;
+  }
+}
+function validaCep() {
+  let txtCep = document.querySelector("#txtCep");
+
+  if (CEP.value == "") {
+    txtCep.innerHTML = "CEP inválido";
+    txtCep.style.color = "red";
+  } else {
+    txtCep.innerHTML = "CEP válido";
+    txtCep.style.color = "green";
+    cepOk = true;
+  }
+}
+function validaEstado() {
+  let txtEstado = document.querySelector("#txtEstado");
+
+  if (estado.value == "") {
+    txtEstado.innerHTML = "Estado inválido";
+    txtEstado.style.color = "red";
+  } else {
+    txtEstado.innerHTML = "Estado válido";
+    txtEstado.style.color = "green";
+    estadoOk = true;
+  }
+}
 
 function enviar() {
   if (nomeOk == true && emailOk == true && assuntoOk == true) {
     alert("Formulário enviado com sucesso!");
+  } else {
+    alert("Preencha os campos do formulário corretamente!");
+  }
+}
+
+function enviar() {
+  if (
+    nomeOk == true &&
+    emailOk == true &&
+    senhaOk == true &&
+    enderecolOk == true &&
+    cidadeOk == true &&
+    cepOk == true &&
+    estadoOk == true
+  ) {
+    alert("Formulário enviado com sucesso!");
+  } else {
+    alert("Preencha os campos do formulário corretamente!");
+  }
+}
+function entrar() {
+  if (
+    nomeOk == true &&
+    emailOk == true &&
+    senhaOk == true &&
+    enderecolOk == true &&
+    cidadeOk == true &&
+    cepOk == true &&
+    estadoOk == true
+  ) {
+    window.location.href = "index.html";
   } else {
     alert("Preencha os campos do formulário corretamente!");
   }
